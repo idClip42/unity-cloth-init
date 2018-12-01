cb8d559a5a81145a47298f7d2c33c2ea1a0ab729
Alexander Earley <axe6801@rit.edu>
2018-11-30 19:26:58 -0500

    Updated cloth init to be entirely within the editor, with a button, run outside of play mode Automatically goes thru different parameter arrangements until it finds one that works Vastly simplified public variables

--- ClothInit.cs
+++ ClothInit.cs
@@ -2,21 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ClothInit : MonoBehaviour 
+public class ClothInit : MonoBehaviour
 {
-	public bool useUv = false;
-	public bool useGradientForUv = false;
-	public bool useRed = true;
-	public float maxDistance = 0.15f;
-	[Range(0,1)] public float uvThreshold = 0.25f;
-	public Animator animator;
-	public bool debugObjs = true;
-	public bool endOnFirstFail = true;
-	public bool oneTestPerVert = false;
-	public bool checkViaMinDistance = true;
-	public float minDistForMatch = 0.001f;
-	public Vector3 compareRotation;
-	public bool useInverseTransformIfNotAligned;
+    //public Animator animator;
+    public enum ClothInitType { UV_Hard, UV_Gradient, Vert_Red, UV_Hard_Vert_Red, UV_Gradient_Vert_Red };
+    public float maxDistance = 0.15f;
+    public ClothInitType clothGenerationType = ClothInitType.Vert_Red;
+    [Range(0, 1)] public float uvThreshold = 0.25f;  // Only show this when UVs are on
+    public float minDistForMatch = 0.001f;          // Only show this when red is on
+
+    bool useUv = false;
+	bool useGradientForUv = false;
+	bool useRed = true;
+
+    //public bool debugObjs = true;
+    const bool debugObjs = false;
+    //public bool endOnFirstFail = true;
+    const bool endOnFirstFail = true;
+    //public bool oneTestPerVert = false;
+    const bool oneTestPerVert = false;
+    //public bool checkViaMinDistance = true;
+    const bool checkViaMinDistance = true;
 	ClothSphereColliderPair[] colliderSpheres;
 	CapsuleCollider[] colliderCapsules;
 
@@ -31,62 +37,141 @@ public class ClothInit : MonoBehaviour
 	float damping;
 	Vector3 animPos;
 
-	void Start () 
+
+    Vector3[] possibleCompareRotations = {
+        new Vector3(0,0,0),
+        new Vector3(90,0,0),
+        new Vector3(-90,0,0)
+    };
+    //Vector3 currentCompareRotation;
+    //bool currentInverseTransform = false;
+    Vector3 compareRotation;
+    bool useInverseTransformIfNotAligned;
+
+
+    //void Start () 
+    public void InitCloth()
 	{
 		Initialize();
 		Prepare();
 		if(debugObjs) DebugShapes();
 		ZeroOut();
 
-		for(int c = 0; c < cloth.vertices.Length; ++c)
-		{
-			int matchingIndex = c;
-			Vector3 clothVert = cloth.vertices[c];
-			Vector3 meshVert = mesh.vertices[c];
+        bool didIt = false;
 
-//			if(TransformClothVert(clothVert) != TransformMeshVert(meshVert))
-			if(!CompareVerts(clothVert, meshVert))
-			{
-				if(oneTestPerVert) continue;
-				for(int m = 0; m < mesh.vertices.Length; ++m)
-				{
-					var mIndex = m;
-					mIndex = (m + c) % mesh.vertices.Length;
-					meshVert = mesh.vertices[mIndex];
-//					if(TransformClothVert(clothVert) == TransformMeshVert(meshVert))
-					if(CompareVerts(clothVert, meshVert))
-					{
-						matchingIndex = mIndex;
-						break;
-					}
-				}
-				if(matchingIndex == c)
-				{
-					Debug.Log("Couldn't find a match");
-					if(endOnFirstFail)
-					{
-						Debug.Log("FAILED! After " + c + " vertices.");
-						Repair();
-						return;
-					}
-					continue;
-				}
-			}
 
-			ApplyClothVert(c, matchingIndex);
-		}
+        //useInverseTransformIfNotAligned = false;
+        for (int c = 0; c < possibleCompareRotations.Length; ++c)
+        {
+            compareRotation = possibleCompareRotations[c];
+            useInverseTransformIfNotAligned = false;
+            if (RunThruMatchingProcess()) 
+            {
+                didIt = true;
+                break;
+            }
+            useInverseTransformIfNotAligned = true;
+            if (RunThruMatchingProcess()) {
+                didIt = true;
+                break;
+            } 
+        }
+
 
 		Repair();
 
-		Debug.Log("UPDATED CLOTH CONSTRAINTS on " + gameObject.name + ". Copy Cloth settings to GameObject out of play mode and turn this script off.");
+        if (didIt)
+            Debug.Log("UPDATED CLOTH CONSTRAINTS on " + gameObject.name + ".");
+        else
+        {
+            Debug.Log("FAILED TO UPDATE CLOTH CONSTRAINTS");
+            //DebugShapes();
+        }
 	}
+
+    bool RunThruMatchingProcess()
+    {
+        for (int c = 0; c < cloth.vertices.Length; ++c)
+        {
+            int matchingIndex = c;
+            Vector3 clothVert = cloth.vertices[c];
+            Vector3 meshVert = mesh.vertices[c];
+
+            //          if(TransformClothVert(clothVert) != TransformMeshVert(meshVert))
+            if (!CompareVerts(clothVert, meshVert))
+            {
+                if (oneTestPerVert) continue;
+                for (int m = 0; m < mesh.vertices.Length; ++m)
+                {
+                    var mIndex = m;
+                    mIndex = (m + c) % mesh.vertices.Length;
+                    meshVert = mesh.vertices[mIndex];
+                    //                  if(TransformClothVert(clothVert) == TransformMeshVert(meshVert))
+                    if (CompareVerts(clothVert, meshVert))
+                    {
+                        matchingIndex = mIndex;
+                        break;
+                    }
+                }
+                if (matchingIndex == c)
+                {
+                    //Debug.Log("Couldn't find a match");
+                    if (endOnFirstFail)
+                    {
+                        //Debug.Log("FAILED! After " + c + " vertices.");
+                        //Repair();
+                        return false;
+                    }
+                    continue;
+                }
+            }
+
+            ApplyClothVert(c, matchingIndex);
+        }
+
+        return true;
+    }
 
 	void Initialize()
 	{
 		smr = GetComponent<SkinnedMeshRenderer>();
 		mesh = smr.sharedMesh;
 		cloth = GetComponent<Cloth>();
-	}
+        SetUsageType();
+    }
+
+    void SetUsageType()
+    {
+        // Can we do this in less lines?
+        switch(clothGenerationType)
+        {
+            case ClothInitType.UV_Gradient:
+                useUv = true;
+                useGradientForUv = true;
+                useRed = false;
+                break;
+            case ClothInitType.UV_Gradient_Vert_Red:
+                useUv = true;
+                useGradientForUv = true;
+                useRed = true;
+                break;
+            case ClothInitType.UV_Hard:
+                useUv = true;
+                useGradientForUv = false;
+                useRed = false;
+                break;
+            case ClothInitType.UV_Hard_Vert_Red:
+                useUv = true;
+                useGradientForUv = false;
+                useRed = true;
+                break;
+            case ClothInitType.Vert_Red:
+                useUv = false;
+                useGradientForUv = false;
+                useRed = true;
+                break;
+        }
+    }
 
 	void Prepare()
 	{
@@ -95,15 +180,15 @@ public class ClothInit : MonoBehaviour
 
 		newConstraints = cloth.coefficients;
 
-		if(animator != null)
-		{
-			animator.enabled = false;
-			animPos = animator.transform.position;
-			animator.transform.position = Vector3.zero;
-		} else {
+		//if(animator != null)
+		//{
+			//animator.enabled = false;
+			//animPos = animator.transform.position;
+			//animator.transform.position = Vector3.zero;
+		//} else {
 			animPos = transform.position;
 			transform.position = Vector3.zero;
-		}
+		//}
 
 		//		Vector3 origScale = transform.lossyScale;
 		//		transform.lossyScale = 1;
@@ -128,13 +213,13 @@ public class ClothInit : MonoBehaviour
 
 		smr.rootBone = rootBone;
 
-		if(animator != null)
-		{
-			animator.enabled = true;
-			animator.transform.position = animPos;
-		} else {
+		//if(animator != null)
+		//{
+			//animator.enabled = true;
+			//animator.transform.position = animPos;
+		//} else {
 			transform.position = animPos;
-		}
+		//}
 
 		cloth.capsuleColliders = colliderCapsules;
 		cloth.sphereColliders = colliderSpheres;
---
+++ Editor/ClothInitEditor.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(ClothInit))]
+public class ClothInitEditor : Editor 
+{
+
+    public override void OnInspectorGUI()
+    {
+        //DrawDefaultInspector();
+
+
+        ClothInit script = (ClothInit)target;
+
+        //script.animator = (Animator)EditorGUILayout.ObjectField(
+            //"Animator",
+            //script.animator,
+            //typeof(Animator),
+            //true);
+
+        script.maxDistance = EditorGUILayout.FloatField("Max Move Distance", script.maxDistance);
+
+        script.clothGenerationType = (ClothInit.ClothInitType)EditorGUILayout.EnumPopup("Cloth Init Type", script.clothGenerationType);
+
+        if (script.clothGenerationType == ClothInit.ClothInitType.UV_Hard ||
+          script.clothGenerationType == ClothInit.ClothInitType.UV_Gradient ||
+           script.clothGenerationType == ClothInit.ClothInitType.UV_Gradient_Vert_Red ||
+           script.clothGenerationType == ClothInit.ClothInitType.UV_Hard_Vert_Red)
+        {
+            script.uvThreshold = EditorGUILayout.Slider("UV Threshold", script.uvThreshold, 0, 1);
+        }
+
+        if (script.clothGenerationType == ClothInit.ClothInitType.Vert_Red ||
+           script.clothGenerationType == ClothInit.ClothInitType.UV_Hard_Vert_Red ||
+           script.clothGenerationType == ClothInit.ClothInitType.UV_Gradient_Vert_Red)
+        { 
+        script.minDistForMatch = EditorGUILayout.FloatField("Min Point Match Distance", script.minDistForMatch);
+        }
+
+
+
+
+        if (GUILayout.Button("Go!"))
+        {
+            //Debug.Log("Went!");
+            script.InitCloth();
+        }
+    }
+}
